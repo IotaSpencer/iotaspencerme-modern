c9c37a8f8e7767751e1ed2f60f069b45f25f66d9
IotaSpencer <me@iotaspencer.me>
2026-08-31 00:46:33 -0400

    feat: add astro-icon package for enhanced icon support in the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,20 +6,22 @@ import remarkAttributes from 'remark-attributes';
 import cloudflare from "@astrojs/cloudflare";
 import { unified } from '@astrojs/markdown-remark';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://new.iotaspencer.me",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
-	markdown: {
-		processor: unified({
-			remarkPlugins: [
-				[remarkAttributes, {}]
-			],
-		}),
-	},
+    site: "https://new.iotaspencer.me",
+    integrations: [mdx(), sitemap(), icon()],
+    adapter: cloudflare({
+        platformProxy: {
+            enabled: true,
+        },
+    }),
+    markdown: {
+        processor: unified({
+            remarkPlugins: [
+                [remarkAttributes, {}]
+            ],
+        }),
+    },
 });
